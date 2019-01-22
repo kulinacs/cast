c63a349206bf2f7866ba9a057f834844051aa00c
Nicklaus McClendon <nicklaus@kulinacs.com>
2019-01-22 08:54:55 -0600

    Switch shell tests to use bytes.Buffer

--- agent/shell_test.go
+++ agent/shell_test.go
@@ -1,24 +1,11 @@
 package agent
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
-	"io"
 	"strings"
 	"testing"
 )
-
-// shellFixture returns a pipe to write to for the shell to read, a pipe to read from to get shell output, and the shell attached
-func shellFixture() (*io.PipeWriter, *io.PipeReader, *Shell) {
-	readPipeReader, readPipeWriter := io.Pipe()
-	writePipeReader, writePipeWriter := io.Pipe()
-	testShell := &Shell{active: true, reader: bufio.NewScanner(readPipeReader),
-		readInternal:    make(chan string, 10),
-		ReadInteractive: make(chan string, 10),
-		writer:          writePipeWriter, WriteInteractive: make(chan string, 10)}
-	return readPipeWriter, writePipeReader, testShell
-}
 
 // TestNewShell tests creating a new shell
 func TestNewShell(t *testing.T) {
@@ -30,10 +17,10 @@ func TestNewShell(t *testing.T) {
 
 // TestRead tests a single non-interactive read
 func TestRead(t *testing.T) {
+	var testBuffer bytes.Buffer
 	testVal := "test text"
-	testShell := &Shell{active: true, reader: bufio.NewScanner(strings.NewReader(testVal + "\n")),
-		readInternal: make(chan string, 10)}
-	go testShell.startReader()
+	testShell := NewShell(&testBuffer, 10)
+	fmt.Fprintf(&testBuffer, testVal+"\n")
 	recvVal, _ := testShell.Read()
 	if recvVal != testVal {
 		t.Errorf("Received value was incorrect, got: %s, want: %s", recvVal, testVal)
@@ -45,10 +32,10 @@ func TestRead(t *testing.T) {
 
 // TestReadAll tests a multiline non-interactive read
 func TestReadAll(t *testing.T) {
+	var testBuffer bytes.Buffer
 	testVal := "test text"
-	testShell := &Shell{active: true, reader: bufio.NewScanner(strings.NewReader(strings.Repeat(testVal+"\n", 5))),
-		readInternal: make(chan string, 10)}
-	go testShell.startReader()
+	testShell := NewShell(&testBuffer, 10)
+	fmt.Fprintf(&testBuffer, strings.Repeat(testVal+"\n", 3))
 	recvVal, err := testShell.ReadAll()
 	if err != nil {
 		t.Errorf("an error occurred reading the pipe")
@@ -62,31 +49,23 @@ func TestReadAll(t *testing.T) {
 
 // TestWrite tests a single non-interactive write
 func TestWrite(t *testing.T) {
-	testVal := "test text~"
-	_, pipeReader, testShell := shellFixture()
-	go func() {
-		testShell.Write(testVal)
-	}()
-	bufferedReader := bufio.NewReader(pipeReader)
-	recvVal, err := bufferedReader.ReadString('~')
-	if err != nil {
-		t.Errorf("an error occurred reading the pipe")
-	}
-	if recvVal != testVal {
+	var testBuffer bytes.Buffer
+	testVal := "test text"
+	testShell := NewShell(&testBuffer, 10)
+	testShell.Write(testVal)
+	recvVal := testBuffer.String()
+	if recvVal != testVal+"\n" {
 		t.Errorf("Received value was incorrect, got: %s, want: %s", recvVal, testVal)
 	}
 }
 
 // TestHandleReadInteractive tests a single interactive read
 func TestHandleReadInteractive(t *testing.T) {
+	var testBuffer bytes.Buffer
 	testVal := "test text"
-	pipeWriter, _, testShell := shellFixture()
-	go testShell.startReader()
-	go func() {
-		fmt.Fprint(pipeWriter, testVal+"\n")
-	}()
-	testShell.interactive = true
-	go testShell.readInteractive()
+	testShell := NewShell(&testBuffer, 10)
+	fmt.Fprintf(&testBuffer, testVal+"\n")
+	testShell.Interactive()
 	recvVal := <-testShell.ReadInteractive
 	if recvVal != testVal {
 		t.Errorf("Received value was incorrect, got: %s, want: %s", recvVal, testVal)
@@ -95,18 +74,16 @@ func TestHandleReadInteractive(t *testing.T) {
 
 // TestHandleWriteInteractive tests a single write from a pipe
 func TestHandleWriteInteractive(t *testing.T) {
-	testVal := "test text~"
-	_, pipeReader, testShell := shellFixture()
+	var testBuffer bytes.Buffer
+	testVal := "test text"
+	testShell := NewShell(&testBuffer, 10)
 	testShell.WriteInteractive <- testVal
-	// Go interactive to test writeInteractive
-	testShell.interactive = true
-	go testShell.writeInteractive()
-	bufferedReader := bufio.NewReader(pipeReader)
-	recvVal, err := bufferedReader.ReadString('~')
-	if err != nil {
-		t.Errorf("an error occurred reading the pipe")
+	testShell.Interactive()
+	recvVal := ""
+	for recvVal == "" {
+		recvVal = testBuffer.String()
 	}
-	if recvVal != testVal {
+	if recvVal != testVal+"\n" {
 		t.Errorf("Received value was incorrect, got: %s, want: %s", recvVal, testVal)
 	}
 }
